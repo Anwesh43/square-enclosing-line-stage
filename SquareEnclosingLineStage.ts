08016ba6cec70523d02653874cbc15ac49091155
Anwesh43 <anweshthecool0@gmail.com>
2019-03-31 17:00:09 +0530

    adding constants

---
+++ SquareEnclosingLineStage.ts
@@ -0,0 +1,10 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const nodes : number = 5
+const lines : number = 2
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#1565C0"
+const backColor : string = "#bdbdbd"
