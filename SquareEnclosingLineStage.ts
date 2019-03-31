4379f2c6d6fb4fdfa1460fecd0460fc3cf1ada63
Anwesh43 <anweshthecool0@gmail.com>
2019-03-31 17:32:57 +0530

    adding SquareEnclosingLineStage class

--- SquareEnclosingLineStage.ts
+++ SquareEnclosingLineStage.ts
@@ -81,3 +81,34 @@ class DrawingUtil {
           context.restore()
       }
 }
+
+class SquareEnclosingLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : SquareEnclosingLineStage = new SquareEnclosingLineStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
