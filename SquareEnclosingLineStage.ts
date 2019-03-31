7fa2489a968e00b6f72802e5e36518436dd3bacd
Anwesh43 <anweshthecool0@gmail.com>
2019-03-31 17:08:08 +0530

    adding ScaleUitl class

--- SquareEnclosingLineStage.ts
+++ SquareEnclosingLineStage.ts
@@ -8,3 +8,26 @@ const strokeFactor : number = 90
 const sizeFactor : number = 2.9
 const foreColor : string = "#1565C0"
 const backColor : string = "#bdbdbd"
+
+class ScaleUtil {
+    static scaleFactor(scale : number) : number {
+        return Math.floor(scale / scDiv)
+    }
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n))
+    }
+
+    static mirrorValue(scale : number, a : number, b : number) : number {
+        const k : number = ScaleUtil.scaleFactor(scale)
+        return (1 - k) / a + k / b
+    }
+
+    static updateValue(scale : number, dir : number, a : number, b : number) : number {
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
+    }
+}
